4823470a9fd09120b519f9bed9a341404d412c46
Sushilkumar <dev.sahanisushilkumar@gmail.com>
2024-09-05 02:46:00 +0530

    multiple files

--- storage/views.py
+++ storage/views.py
@@ -8,20 +8,23 @@ from admin.decorator import api_key_required
 from django.views.decorators.csrf import csrf_exempt
 
 
-
-
 @api_view(['POST'])
 @api_key_required
 @csrf_exempt
-def upload_file(request):
+def upload_files(request):
     base_url = request.get_host()
     if 'file' not in request.FILES:
-        return Response({'error': 'No file provided'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error': 'No file(s) provided'}, status=status.HTTP_400_BAD_REQUEST)
 
-    file = request.FILES['file']
-    random_name = str(uuid.uuid4()) + '_' + file.name
-    uploaded_file = UploadedFile(file=file)
-    uploaded_file.save()
-    file_path = uploaded_file.file.url
-    file_url = "http://"+ base_url + file_path
-    return Response({'file_url': file_url}, status=status.HTTP_201_CREATED)
+    file_urls = []
+    
+    # Loop through each file in the request
+    for file in request.FILES.getlist('file'):
+        random_name = str(uuid.uuid4()) + '_' + file.name
+        uploaded_file = UploadedFile(file=file)
+        uploaded_file.save()
+        file_path = uploaded_file.file.url
+        file_url = "http://" + base_url + file_path
+        file_urls.append(file_url)
+
+    return Response({'file_urls': file_urls}, status=status.HTTP_201_CREATED)
